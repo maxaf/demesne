fc815fbf176b78aae7c98a3d417ab97ddb52aadc
Max Afonov <max@bumnetworks.com>
2017-06-06 18:59:43 +0000

    install coursier binary

--- Dockerfile.scala
+++ Dockerfile.scala
@@ -6,15 +6,19 @@ FROM demesne
 ARG SCALA_VERSION=2.12.2
 ENV SCALA_HOME /usr/local/scala-${SCALA_VERSION}
 ENV SBT $SCALA_HOME/bin/sbt
+ENV COURSIER $SCALA_HOME/bin/coursier
 
 RUN sudo mkdir -p $SCALA_HOME
 
 RUN sudo apt-get install -y openjdk-8-jdk-headless
-RUN curl -sk https://downloads.lightbend.com/scala/${SCALA_VERSION}/scala-${SCALA_VERSION}.tgz \
+RUN curl -s https://downloads.lightbend.com/scala/${SCALA_VERSION}/scala-${SCALA_VERSION}.tgz \
     | sudo tar zxf - -C $SCALA_HOME --strip-components=1
 
-RUN sudo curl -sk -o $SBT https://raw.githubusercontent.com/paulp/sbt-extras/master/sbt \
+RUN sudo curl -s -o $SBT https://raw.githubusercontent.com/paulp/sbt-extras/master/sbt \
     && sudo chmod 755 $SBT
+
+RUN sudo curl -sL -o $COURSIER https://git.io/vgvpD \
+    && sudo chmod 755 $COURSIER
 
 ENV PATH $SCALA_HOME/bin:$PATH
 
