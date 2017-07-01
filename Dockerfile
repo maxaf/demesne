ba23623e156ec31e0f278f2f7f492146f525603b
Max Afonov <max@bumnetworks.com>
2017-07-01 03:42:44 +0000

    replaced docker-in-docker with a shared socket that reuses host docker daemon

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,14 @@ FROM debian:unstable
 ARG USER
 ARG UID
 ARG GID
+ARG DOCKER_GID
 
 ENV USER=$USER
 ENV HOME /home/$USER
 
 RUN apt-get update \
     && apt-get dist-upgrade -y \
-    && apt-get install -y sudo apt-utils libterm-readline-gnu-perl curl \
+    && apt-get install -y sudo apt-utils libterm-readline-gnu-perl curl docker.io \
     && apt-get autoremove \
     && apt-get autoclean
 
@@ -17,7 +18,9 @@ RUN getent group $GID >/dev/null || addgroup --gid $GID $USER \
     && adduser --quiet --system --home $HOME --uid $UID --gid $GID --disabled-login $USER \
     && echo $USER:password | chpasswd \
     && sudo adduser $USER sudo \
-    && echo '%sudo ALL=(ALL:ALL) NOPASSWD: ALL' >/etc/sudoers.d/demesne
+    && echo '%sudo ALL=(ALL:ALL) NOPASSWD: ALL' >/etc/sudoers.d/demesne \
+    && groupmod -o -g $DOCKER_GID docker \
+    && usermod -G docker -a $USER
 
 WORKDIR $HOME
 USER $USER
--- Dockerfile.scala
+++ Dockerfile.scala
@@ -10,8 +10,7 @@ ENV COURSIER $SCALA_HOME/bin/coursier
 
 RUN sudo mkdir -p $SCALA_HOME
 
-RUN sudo apt install -y openjdk-8-jdk-headless git docker.io procps xterm
-RUN sudo usermod -G docker -a $USER
+RUN sudo apt install -y openjdk-8-jdk-headless git procps xterm
 
 RUN curl -s https://downloads.lightbend.com/scala/${SCALA_VERSION}/scala-${SCALA_VERSION}.tgz \
     | sudo tar zxf - -C $SCALA_HOME --strip-components=1
@@ -25,15 +24,6 @@ RUN sudo curl -sL -o $COURSIER https://git.io/vgvpD \
 ENV PATH $SCALA_HOME/bin:$PATH
 
 RUN mkdir -p $HOME/.sbt/0.13/plugins \
-    && echo 'addSbtPlugin("io.get-coursier" % "sbt-coursier" % "1.0.0-RC3")' >$HOME/.sbt/0.13/plugins/build.sbt
+    && echo 'addSbtPlugin("io.get-coursier" % "sbt-coursier" % "1.0.0-RC6")' >$HOME/.sbt/0.13/plugins/build.sbt
 
-ENV WD /usr/local/bin/wrapdocker
-ENV WD_URL https://raw.githubusercontent.com/jpetazzo/dind/master/wrapdocker
-RUN sudo curl -sL -o $WD $WD_URL && sudo chmod 755 $WD
-
-ENV INTELLIJ_IDEA_URL https://download-cf.jetbrains.com/idea/ideaIC-2017.1.4-no-jdk.tar.gz
-ENV INTELLIJ_IDEA_HOME $HOME/.intellij-idea-home
-RUN mkdir $INTELLIJ_IDEA_HOME && \
-    curl -sL $INTELLIJ_IDEA_URL | tar zvxf - -C $INTELLIJ_IDEA_HOME
-
-ENTRYPOINT ["sudo", "/usr/local/bin/wrapdocker", "sudo", "-u", "demesne", "/bin/cat"]
+ENTRYPOINT ["/bin/cat"]
